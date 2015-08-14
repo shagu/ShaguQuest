795b8204fab6d9ccc4d456216084ea692d328d03
Eric Mauser <ericmauser@googlemail.com>
2015-08-14 14:55:47 +0200

    added german localemix build

--- build.sh
+++ build.sh
@@ -4,17 +4,23 @@ VERSION="4.2"
 
 echo ":: cleaning up"
 rm -rf build
-mkdir -p build/enGB/ShaguQuest build/deDE/ShaguQuest release
+mkdir -p build/enGB/ShaguQuest build/deDE/ShaguQuest build/deMIX/ShaguQuest release
 
 echo ":: installing external addons"
 cp -rf resources/Cartographer build/deDE
+cp -rf resources/Cartographer build/deMIX
 cp -rf resources/Cartographer build/enGB
 
 cp -rf resources/EQL3 build/deDE
+cp -rf resources/EQL3 build/deMIX
 cp -rf resources/EQL3 build/enGB
 
 # patch locales for german cartographer to avoid lua erros
 cd build/deDE
+patch -s -p 1 < ../../resources/Cartographer_deDE.patch
+cd - >> /dev/null
+
+cd build/deMIX
 patch -s -p 1 < ../../resources/Cartographer_deDE.patch
 cd - >> /dev/null
 
@@ -30,17 +36,26 @@ cp resources/spawnData.lua_enGB build/enGB/ShaguQuest/spawnData.lua
 cp resources/itemData.lua_enGB build/enGB/ShaguQuest/itemData.lua
 cp resources/questData.lua_enGB build/enGB/ShaguQuest/questData.lua
 
+cp -rf resources/zoneData.lua_deDE build/deMIX/ShaguQuest/zoneData.lua
+cp resources/spawnData.lua_enGB build/deMIX/ShaguQuest/spawnData.lua
+cp resources/itemData.lua_enGB build/deMIX/ShaguQuest/itemData.lua
+cp resources/questData.lua_enGB build/deMIX/ShaguQuest/questData.lua
+
+
 # install default files
 cp -rf resources/symbols ShaguQuest.lua ShaguQuest.toc ShaguQuest.xml build/deDE/ShaguQuest
 cp -rf resources/symbols ShaguQuest.lua ShaguQuest.toc ShaguQuest.xml build/enGB/ShaguQuest
+cp -rf resources/symbols ShaguQuest.lua ShaguQuest.toc ShaguQuest.xml build/deMIX/ShaguQuest
 
 # replace veresion string
 sed -i "s/oooVersionooo/$VERSION/g" build/deDE/ShaguQuest/ShaguQuest.*
 sed -i "s/oooVersionooo/$VERSION/g" build/enGB/ShaguQuest/ShaguQuest.*
+sed -i "s/oooVersionooo/$VERSION/g" build/deMIX/ShaguQuest/ShaguQuest.*
 
 # replace locale string
 sed -i "s/oooLocaleooo/deDE/g" build/deDE/ShaguQuest/ShaguQuest.*
 sed -i "s/oooLocaleooo/enGB/g" build/enGB/ShaguQuest/ShaguQuest.*
+sed -i "s/oooLocaleooo/enGB/g" build/deMIX/ShaguQuest/ShaguQuest.*
 
 echo ":: building release zip"
 echo "   - deDE"
@@ -53,4 +68,7 @@ cd build/enGB
 zip -qr9 ../../release/ShaguQuest-$VERSION-enGB-complete.zip *
 cd - >> /dev/null
 
-
+echo "   - deMIX"
+cd build/deMIX
+zip -qr9 ../../release/ShaguQuest-$VERSION-deMIX-complete.zip *
+cd - >> /dev/null
